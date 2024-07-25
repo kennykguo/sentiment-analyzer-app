23e624d73ae6f7eb375fad0926100abc88e1db8f
Kenny <kennykguo@gmail.com>
2024-07-24 21:40:58 -0400

    feat: finished django rest tutorial

--- django-tests/django_rest_tutorial/snippets/serializers.py
+++ django-tests/django_rest_tutorial/snippets/serializers.py
@@ -28,7 +28,22 @@ from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 #         instance.save()
 #         return instance
 
-class SnippetSerializer(serializers.ModelSerializer):
+
+from django.contrib.auth.models import User
+
+class SnippetSerializer(serializers.HyperlinkedModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+    highlight = serializers.HyperlinkedIdentityField(view_name='snippet-highlight', format='html')
+
     class Meta:
         model = Snippet
-        fields = ['id', 'title', 'code', 'linenos', 'language', 'style']
+        fields = ['url', 'id', 'highlight', 'owner',
+                  'title', 'code', 'linenos', 'language', 'style']
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    snippets = serializers.HyperlinkedRelatedField(many=True, view_name='snippet-detail', read_only=True)
+
+    class Meta:
+        model = User
+        fields = ['url', 'id', 'username', 'snippets']
